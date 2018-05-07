e92e7c1bb530305bffee0598d63be4cf3239cb38
Macarena Solange Quiroga <msinf.quiroga@gmail.com>
2018-05-07 02:29:43 -0300

    v0.0 Componente RoboPava Rápida

---
+++ src/root/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HeatFast from './HeatFast';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeatFast/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/root/HeatFast.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './css/heatFast.css';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import {Button} from 'material-ui'; 
+import InputTemperature from './InputTemperature';
+
+const styles = theme => ({
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+      width: 200,
+    }
+  });
+class HeatFast extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            temperatura:"0"
+        }
+    }
+    render() {
+    const { classes } = this.props;
+    return (
+    <Grid container spacing={16}>
+        <Grid item xs={12}>
+            <div className="heatFast">
+                <h1> RoboPava rápida </h1>
+            </div>
+        </Grid>
+        <Grid item xs={12}>
+            <InputTemperature value={this.state.temperatura}/>
+            <Button variant="raised" size="medium" color="primary" className={classes.button}>
+              Calentar
+            </Button>
+        </Grid>
+    </Grid>
+    );
+    }
+}
+HeatFast.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles) (HeatFast);
